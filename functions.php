42fa2c40ff1b41763cf4fcf267a5cc56b349f1b8
brangiojitu <72190604+brangiojitu@users.noreply.github.com>
2021-11-21 05:52:31 +0200

    Delete functions.php

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function zerohunger_enqueue_style() {
-    wp_enqueue_style("zerohunger-style", get_stylesheet_uri());
-}
-add_action("wp_enqueue_scripts", "zerohunger_enqueue_style");
-
-?>
